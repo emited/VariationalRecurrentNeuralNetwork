79c8603ac0047e0f873e8a0894e190ad4953a178
Aniekan Umoren <anixmc1@gmail.com>
2021-11-30 12:31:39 -0500

    gpu and minor changes

--- sample.py
+++ sample.py
@@ -9,10 +9,14 @@ h_dim = 100
 z_dim = 16
 n_layers =  1
 
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+
 state_dict = torch.load('saves/vrnn_state_dict_41.pth')
 model = VRNN(x_dim, h_dim, z_dim, n_layers)
 model.load_state_dict(state_dict)
+model.to(device)
 
 sample = model.sample(28*6)
-plt.imshow(sample.numpy(), cmap='gray')
+plt.imshow(sample.cpu().numpy(), cmap='gray')
 plt.show()
